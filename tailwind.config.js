ba99d16c6371a9a45b9463c8231f54d4c5895886
Kaishiyoku <dmp@nfze.de>
2020-09-02 22:56:05 +0200

    added styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,47 @@ module.exports = {
     './src/**/*.jsx',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Concourse'],
+      },
+      aspectRatio: {
+        none: 0,
+        square: [1, 1],
+        "16/9": [16, 9],
+        "4/3": [4, 3],
+        "21/9": [21, 9],
+      },
+      fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+      },
+      fontWeight: {
+        hairline: '100',
+        thin: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
+    },
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    aspectRatio: ['responsive']
+  },
+  plugins: [
+    require("tailwindcss-responsive-embed"),
+    require("tailwindcss-aspect-ratio"),
+  ],
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,12 @@
 const mix = require('laravel-mix');
 
 mix.postCss('src/css/style.css', 'dist/css', [
-    require('tailwindcss'),
-]);
+        require('tailwindcss'),
+    ])
+    .sass('src/scss/concourse.scss', 'dist/css')
+    .copy('src/img', 'dist/img')
+    .copy('src/fonts', 'dist/fonts')
+    .copy('src/index.html', 'dist/index.html')
+    .options({
+        processCssUrls: false
+    });
